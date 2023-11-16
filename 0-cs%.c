6e389ba221222b2f277057c480363f2c39e5ce95
Bayan445 <bayan.khaled.04@gmail.com>
2023-11-16 09:59:25 -0800

    iraq

--- 0-cs%.c
+++ 0-cs%.c
@@ -21,7 +21,7 @@ void printString(char *str, int *count)
 	if (str == NULL)
 	{
 		write(1, "(null)", 6);
-		count += 6;
+		(*count) += 6;
 	}
 
 	/*
@@ -32,7 +32,7 @@ void printString(char *str, int *count)
 	}
 	*/
 
-	
+
 	while (*str != '\0')
 	{
 		putchar(*str);
@@ -65,12 +65,27 @@ void handleFormatSpecifier(char specifier, va_list thearguments, int *count)
 	case 'c':
 		printChar(va_arg(thearguments, int), count);
 		break;
+
 	case 's':
-		printString(va_arg(thearguments, char*), count);
+		
+		char *str = va_arg(thearguments, char*);
+		if (str == NULL)
+		{
+			write(1, "(null)", 6);
+			(*count) += 6;
+		}
+		else
+		{
+			write(1, str, strlen(str));
+			(*count) += strlen(str);
+		}
 		break;
+	
 	case '%':
+		
 		printPercent(count);
 		break;
+	
 	default:
 		putchar(specifier);
 		(*count)++;
