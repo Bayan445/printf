22615abc63006a98132c5afe7260e7b50f72172f
Bayan445 <bayan.khaled.04@gmail.com>
2023-11-16 00:10:22 -0800

    naffeer

--- main.h
+++ main.h
@@ -5,5 +5,10 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+void printChar(int character, int *count);
+void printString(char *str, int *count);
+void printPercent(int *count);
+void handleFormatSpecifier(char specifier, va_list thearguments, int *count);
+
 
 #endif
