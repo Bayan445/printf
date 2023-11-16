4d0ab7e10d050af426c8466f05a71dc555e7a087
Bayan445 <bayan.khaled.04@gmail.com>
2023-11-16 10:07:42 -0800

    ouf

--- 0-cs%.c
+++ 0-cs%.c
@@ -67,9 +67,9 @@ void handleFormatSpecifier(char specifier, va_list thearguments, int *count)
 	case 'c':
 		printChar(va_arg(thearguments, int), count);
 		break;
-		
+
 	case 's':
-		
+
 		printString(va_arg(thearguments, char*), count);
 		break;
 
