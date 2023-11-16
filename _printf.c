dc54f5106d4a49d90cb2c34d6e6dd2fa3d0fc08c
Bayan445 <bayan.khaled.04@gmail.com>
2023-11-16 00:36:39 -0800

    yyyyyyyyyyyyyyyyye

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _printf - A modified function like printf.
+ * @format: The format string we will process.
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+        int count;
+        va_list thearguments;
+
+        va_start(thearguments, format);
+
+        count = 0;
+
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        handleFormatSpecifier(*format, thearguments, &count);
+                }
+                else
+                {
+                        putchar(*format);
+                        count++;
+                }
+
+                format++;
+        }
+
+        va_end(thearguments);
+        return (count);
+}
+
